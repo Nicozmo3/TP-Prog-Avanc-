939c8e82f846ed0dbbd6d074adacfabdf7e3ddd0
Nicolas <nicolas@briere.org>
2026-06-07 17:43:14 +0200

    catalogue

---
+++ catalogue/Produit.java
@@ -0,0 +1,63 @@
+package catalogue;
+
+/**
+ * @author ouziri
+ * @version 0.1
+ *
+ */
+
+public class Produit {
+	private int numero;
+	private String nom;
+	private String categorie;
+	private static int numeroAuto = 1; 
+	
+	public Produit() {	
+	}
+
+	public Produit(String nom, String categorie) {		
+		this.nom = nom;
+		this.categorie = categorie;
+	}
+
+	public static int nextSequence() {
+		return numeroAuto ++;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public String getCategorie() {
+		return categorie;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public void setCategorie(String categorie) {
+		this.categorie = categorie;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (super.equals(obj))
+			return true;
+		try {
+			Produit pObj = (Produit) obj;
+			return this.numero == pObj.getNumero();
+		}
+		catch (Exception e) {
+			return false;
+		}
+	}
+}
